499006d1f65d36cb4262178868b02f4202f104cd
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-04-10 10:38:00 -0300

    classes para conexão adicionadas

--- Aula_integracao/src/main/java/Main.java
+++ Aula_integracao/src/main/java/Main.java
@@ -1,2 +1,16 @@
+import DAO.UserDAO;
+import Model.User;
 public class Main {
+    public static void main(String[] args) {
+
+        UserDAO userDAO = new UserDAO();
+
+        User user = new User("Matheus", "1414789");
+        User user1 = new User("Pedro", "123465");
+
+        // adicionando usuários na tebela do BD
+        userDAO.insertUser(user);
+        userDAO.insertUser(user1);
+
+    }
 }
