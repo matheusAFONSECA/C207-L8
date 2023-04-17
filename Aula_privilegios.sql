1d620754658398074fd68a7fa9f0fd72de4b5744
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-04-17 10:12:14 -0300

    Aula sobre privilegios

---
+++ Aula_privilegios.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS C207;
+CREATE DATABASE C207;
+
+USE C207;
+
+CREATE TABLE alunos(
+	matricula INT,
+    curso VARCHAR(45),
+    nome VARCHAR(45),
+    periodo INT,
+    PRIMARY KEY(matricula)
+);
+
+CREATE USER 'Monitor' IDENTIFIED BY '1111';
+CREATE USER 'Professor' IDENTIFIED BY '2222';
+
+GRANT SELECT, ALTER, DROP, UPDATE, 
+ DELETE, CREATE, INSERT ON C207.alunos TO 'Monitor';
+ 
+ GRANT ALL PRIVILEGES ON C207.* TO 'Professor';
+ 
+ REVOKE INSERT on C207.alunos FROM 'Monitor';
+ 
+ REVOKE CREATE, ALTER, DROP ON C207.* FROM 'Professor';
+ 
+ SHOW GRANTS FOR 'Monitor';
