a282bb173c504a5d5d52d0ac127db54828271242
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2023-05-08 14:30:54 -0300

    Atividade 3 - comandos DDL - exercício quase pronto

--- ATV3.sql
+++ ATV3.sql
@@ -21,7 +21,7 @@ USE `mydb` ;
 -- Table `mydb`.`Agente`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`Agente` (
-  `idAgente` INT NOT NULL,
+  `idAgente` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NOT NULL,
   `endereco` VARCHAR(45) NULL,
   `nascimento` DATE NULL,
@@ -31,12 +31,11 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Agente` (
   PRIMARY KEY (`idAgente`))
 ENGINE = InnoDB;
 
-
 -- -----------------------------------------------------
 -- Table `mydb`.`vilao`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`vilao` (
-  `idvilao` INT NOT NULL,
+  `idvilao` INT NOT NULL AUTO_INCREMENT,
   `nome` VARCHAR(45) NOT NULL,
   `num_crimes` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`idvilao`))
@@ -47,14 +46,13 @@ ENGINE = InnoDB;
 -- Table `mydb`.`missao`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`missao` (
-  `idmissao` INT NOT NULL,
+  `idmissao` INT NOT NULL AUTO_INCREMENT,
   `data` DATE NOT NULL,
   `nome` VARCHAR(45) NOT NULL,
   `local` VARCHAR(45) NULL,
   `duracao` INT NULL,
   `vilao_idvilao` INT NOT NULL,
   PRIMARY KEY (`idmissao`),
-  INDEX `fk_missao_vilao_idx` (`vilao_idvilao` ASC) VISIBLE,
   CONSTRAINT `fk_missao_vilao`
     FOREIGN KEY (`vilao_idvilao`)
     REFERENCES `mydb`.`vilao` (`idvilao`)
@@ -70,8 +68,6 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Agente_has_missao` (
   `Agente_idAgente` INT NOT NULL,
   `missao_idmissao` INT NOT NULL,
   PRIMARY KEY (`Agente_idAgente`, `missao_idmissao`),
-  INDEX `fk_Agente_has_missao_missao1_idx` (`missao_idmissao` ASC) VISIBLE,
-  INDEX `fk_Agente_has_missao_Agente1_idx` (`Agente_idAgente` ASC) VISIBLE,
   CONSTRAINT `fk_Agente_has_missao_Agente1`
     FOREIGN KEY (`Agente_idAgente`)
     REFERENCES `mydb`.`Agente` (`idAgente`)
@@ -88,3 +84,30 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+# AQUI COMEÇA A INSERÇÃO DE DADOS NAS TEBELAS
+
+INSERT INTO agente values 
+('Vivi', 'Conceição-dos-Ouros', '2003-05-08', 'finanças', 'feminino', 'vivi@agente.com'),
+('Juju', 'Zoro', '2000-05-08', 'porradaria', 'feminino', 'juju@agente.com'),
+('Char', 'Paraíso', '1982-05-08', 'roubar', 'feminino', 'char@agente.com'),
+('Vini', 'Santa Rita', '2003-05-08', 'força', 'masculino', 'vini@agente.com'),
+('Gad', 'Poços', '2003-05-08', 'piloto-carro', 'masculino', 'gad@agente.com');
+
+INSERT INTO vilao values 
+("Estevan", 2000),
+("LF", 400),
+("Japa", 4000),
+("Marcelo", 300),
+("Renan", 5000);
+
+INSERT INTO missao values
+('2024-12-08', 'missao finanças', 'Santa-Rita', 2, 1),
+('2024-06-08', 'missao roubo', 'Pouso Alegre', 4, 3),
+('2024-05-08', 'missao piloto', 'São Paulo', 6, 2),
+('2024-07-08', 'missao briga', 'Rio de Janeiro', 7, 5),
+('2024-09-08', 'missao fuga', 'Belo Horizonte', 1, 4);
+
+INSERT INTO agente_has_missao values
+
+
